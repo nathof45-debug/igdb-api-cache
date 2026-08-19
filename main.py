4fb386fe7ee2c91e731c55c53c2a9f0e169a5d1e
nathof45-debug <nathof45@gmail.com>
2026-08-19 09:12:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -185,7 +185,7 @@ print("\n📡 Génération : Sorties populaires de la semaine...")
 query_upcoming = (
     f"{COMMON_FIELDS} "
     f"where first_release_date > {today} & first_release_date <= {next_week} "
-    f"& hypes >= 7 & status != (6, 7); " # Filtre les jeux annulés ou simples rumeurs
+    f"& hypes >= 7 | status != (6, 7); " # Filtre les jeux annulés ou simples rumeurs
     f"sort first_release_date asc; "
     f"limit 500;"
 )
