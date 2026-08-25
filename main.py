0e072510a867fed3a752a55425c67e5f3dadf985
nathof45-debug <nathof45-debug@users.noreply.github.com>
2026-08-25 13:01:02 +0000

    deploy: f2eb8edbb8272b61d00b8703ed952bfe7bba245f

--- main.py
+++ main.py
@@ -87,7 +87,8 @@ def clean_games_data(games_data, scores_dict=None):
                     "y": rd.get("y"),                # Récupère l'année (ex: 2027)
                     "m": rd.get("m"),                # Récupère le mois
                     "d": datetime.datetime.fromtimestamp(rd.get("date")).day if rd.get("date") else None,
-                    "date": rd.get("date")
+                    "date": rd.get("date"),
+                    "status": rd.get("status")
                 } 
                 for rd in game.get("release_dates", [])
             ]
